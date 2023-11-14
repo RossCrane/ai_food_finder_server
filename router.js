80668fe4d2d53a74f43da6ea22d8e571906f6793
knaughtt <dylanrcrane@gmail.com>
2023-11-14 15:23:35 -0700

    test function

--- router.js
+++ router.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const Controller = require('./controllers/controller.js');
 
 router.use(express.json());
+router.get('/', (req, res) => res.send('hello world'));
 router.post('/completions', Controller.getOptions);
 router.post('/user/preferences', Controller.updateUserPreferences);
 router.get('/user/preferences', Controller.getUserPreferences);
